6b7d096cdcc5adfc354e921d732f96f4c85d7879
Maryna-Korbet <m22v86k@gmail.com>
2022-07-24 07:25:05 +0300

    added image markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function makeListItems (images) {
+    return images.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link"
+                        href=${original}>
+                            <img class="gallery__image"
+                            src=${preview}
+                            data-source=${original}
+                            alt=${description} 
+                            />
+                    </a>
+                </div>`
+    })
+        .join("");
+};
+
+galleryContainer.insertAdjacentHTML('beforeend', makeListItems(galleryItems));
+
