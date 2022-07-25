8d9d6b748145853cb11207bab736c2b4a3084815
Maryna-Korbet <m22v86k@gmail.com>
2022-07-25 21:38:10 +0300

    finished task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,16 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
 
-const galleryContainer = document.querySelector('.gallery');
+const galleryContainer = document.querySelector('.gallery'); 
+    
+galleryContainer.insertAdjacentHTML('beforeend', makeListItems(galleryItems));
+galleryContainer.addEventListener('click', onImageClick);
 
-function makeListItems (images) {
+
+function makeListItems(images) {
     return images.map(({ preview, original, description }) => {
         return `<div class="gallery__item">
                     <a class="gallery__link"
@@ -19,7 +24,26 @@ function makeListItems (images) {
                 </div>`
     })
         .join("");
-};
+}
 
-galleryContainer.insertAdjacentHTML('beforeend', makeListItems(galleryItems));
+function onImageClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    openModalWindow(event);
+}
 
+function openModalWindow(event) {   
+    const bigImageURL = event.target.dataset.source;
+    const modal = basicLightbox.create(`
+    <img src="${bigImageURL}" width="800" height="600">`);
+    modal.show(); 
+
+    window.addEventListener('keydown', event => {
+        if (event.code !== 'Escape') {
+            return;
+        }
+        modal.close();
+    });
+}
