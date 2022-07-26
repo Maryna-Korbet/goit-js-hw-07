8c45dab0b77834b184b1519f2490813fb0e493c2
Maryna-Korbet <m22v86k@gmail.com>
2022-07-26 05:00:43 +0300

    tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,10 @@
+// 1. Creating and rendering markup from the galleryItems data array and provided gallery item template.
+// 2. Implementing delegation to div.gallery and getting the url of a large image.
+// 3. Adding the script and styles of the modal window library basicLightbox. Use the jsdelivrCDN service and add links to minified (.min) library files to your project.
+// 4. Opening a modal window by clicking on a gallery item. To do this, check out the documentation and examples.
+// 5. Replacing the value of the src attribute of the <img> element in a modal window before opening. Use the ready-made modal window markup with the image from the examples of the basicLightbox library.
+// 6. Add modal window closing upon pressing the Escape key. Make keyboard listening available only while the modal window is open. In the basicLightbox library, there is a method to close the modal window programmatically.
+
 import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
